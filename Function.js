5bb3996594282f7ada9a70de6a33ccd1dea95fdc
faithmumbi <faithmumbi544@gmail.com>
2018-03-03 08:50:43 +0300

    learned Functions

---
+++ Function.js
@@ -0,0 +1,29 @@
+// this is a void function
+function add (){
+	var numberOne =10;
+	var numberTwo =20;
+
+	var sum = numberOne + numberTwo;
+
+		console.log(sum)
+
+
+}
+
+// call the function
+add()
+
+function multiply(){
+	var numberOne =5;
+	var numberTwo =6;
+	var numberThree =8;
+
+	var answer = numberOne*numberTwo*numberThree;
+
+	return answer;
+
+
+}
+
+var result = multiply()
+console.log(result)
