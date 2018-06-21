86d9fdcaef1a424878cef3a0e3f88d10fc361bc4
faithmumbi <faithmumbi544@gmail.com>
2018-06-21 17:31:07 +0300

    added fatArrow function

---
+++ fatArrow.js
@@ -0,0 +1,8 @@
+var addition= (a,b,c) => a+b+c;
+console.log(addition(10,45,23))
+
+var subtract= (x,y) => x-y;
+console.log(subtract(23,11))
+
+var multiply=(x,y,z) => x*y*z;
+console.log(multiply(4,5,7))
